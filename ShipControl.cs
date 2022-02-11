4e313a95af769fa7730bb6bc925c46f8e8efecdf
Carlos Bonachea <94661538+Cbonachea@users.noreply.github.com>
2022-02-10 16:03:53 -0800

    Add files via upload

--- ShipControl.cs
+++ ShipControl.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(PlayerInput))]
@@ -15,10 +16,14 @@ public class ShipControl : MonoBehaviour
     public Transform cargoDropLocation;
     public SetDeliveryTarget setDeliveryTarget;
     public bool setNewTarget;
+    public float currentFuel;
+    public float fuelConsumption;
+    public Slider slider;
 
     private AreaEffector2D rocketBlast;
     private Animator animator;
     private Rigidbody2D ship;
+    private bool isCoroutineStarted = false;
 
 
     private PlayerInput playerInput;
@@ -36,33 +41,55 @@ public class ShipControl : MonoBehaviour
         rocketBlast = GetComponentInChildren<AreaEffector2D>();
         hasCargo = false;
         setNewTarget = true;
+        currentFuel = 35;
+        fuelConsumption = .2f;
     }
+
+    void Update()
+    {
+        SetFuel(currentFuel);
+    }
+
 
     void FixedUpdate()
     {
-        if (playerInput.thrustInput == true)
+        if (playerInput.thrustInput == true && currentFuel > 0)
         {
             animator.SetBool("isThrusting", true);
             ship.AddForce(transform.up * thrust);
             rocketBlast.enabled = true;
             FindObjectOfType<AudioManager>().Play("EngineSound");
+            if (!isCoroutineStarted)
+            {
+                StartCoroutine(FuelDrainOverTimeCoroutine(fuelConsumption));
+            }
         }
 
         if (playerInput.thrustInput == false)
-        {animator.SetBool("isThrusting", false);
-         FindObjectOfType<AudioManager>().Stop("EngineSound");
-         rocketBlast.enabled = false;}
+        {
+            animator.SetBool("isThrusting", false);
+            FindObjectOfType<AudioManager>().Stop("EngineSound");
+            rocketBlast.enabled = false;
+            StopCoroutine(FuelDrainOverTimeCoroutine(fuelConsumption));
+            isCoroutineStarted = false;
+        }
 
         if (playerInput.torqueInputR == true)
-        {ship.AddTorque(-torque * Mathf.Deg2Rad, ForceMode2D.Impulse);}
+            {ship.AddTorque(-torque * Mathf.Deg2Rad, ForceMode2D.Impulse);}
 
         if (playerInput.torqueInputL == true)
-        {ship.AddTorque(torque * Mathf.Deg2Rad, ForceMode2D.Impulse);}
+            {ship.AddTorque(torque * Mathf.Deg2Rad, ForceMode2D.Impulse);}
 
         if (playerInput.cargoDrop == true && hasCargo == true)
-        {Instantiate(cargo, cargoDropLocation.position , cargoDropLocation.rotation );
-        hasCargo = false;
-        setNewTarget = true;}
+            {Instantiate(cargo, cargoDropLocation.position , cargoDropLocation.rotation );
+            hasCargo = false;
+            setNewTarget = true;}
+
+        if(currentFuel <= 0)
+        {
+            animator.SetBool("isThrusting", false);
+            FindObjectOfType<AudioManager>().Stop("EngineSound");
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -72,6 +99,26 @@ public class ShipControl : MonoBehaviour
             hasCargo = true;
             setDeliveryTarget.SetTarget();
             setNewTarget = false;
+        }
+    }
+
+    public void SetFuel(float currentFuel)
+    {
+        slider.value = currentFuel;
+    }
+
+    public void FuelDrainOverTime(float fuelConsumption)
+    {
+        StartCoroutine(FuelDrainOverTimeCoroutine(fuelConsumption));
+    }
+
+    IEnumerator FuelDrainOverTimeCoroutine(float fuelConsumption)
+    {
+        while (currentFuel > 0 && playerInput.thrustInput == true)
+        {
+            currentFuel -= fuelConsumption;
+            isCoroutineStarted = true;
+            yield return new WaitForSeconds(.1f);
         }
     }
 }
