761e657807647e3207e13e5344f573a740436493
Carlos Bonachea <cbonacheaiv@gmail.com>
2022-04-06 22:22:22 -0700

    reworking everything & breaking SetFuel()

---
+++ Assets/Scripts/Ship/FuelSystem.cs
@@ -0,0 +1,137 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+ 
+public class FuelSystem : MonoBehaviour
+{
+    //PUBLIC VARIABLES
+    // maxFuel sets how much fuel a given tank can carry
+    public int maxFuel;
+    //currentFuel refers to the amount of fuel in your tank
+    public int currentFuel;
+    //engineConsumption is the multiplier for how fast your engine consumes fuel on paper
+    public float engineConsumption;
+    //fuelConsumption is the multiplier for how fast your engine actually consumes fuel
+    public int fuelConsumption;
+    //isFuelConsumptionStarted is used to make sure that the fuel consumption only starts one time
+    public bool isFuelConsumptionStarted;
+    //fuelRefill is the multiplier for how fast your fuel pumps can fill your fuel tank on paper
+    public float fuelPumpRate;
+    //fuelRefill is the multiplier for how fast your fuel pumps can actually fill your fuel tank
+    public int fuelRefill;
+    //slider is used to display current fuel level in the UI
+    public Slider slider;
+
+
+    //PRIVATE VARIABLES
+    //reference to the ShipControl script
+    private ShipControl shipControl;
+    //reference to the PlayerInput script
+    private PlayerInput playerInput;
+    //isFuelRefillStarted is used to make sure that the fuel refill only starts one time
+    private bool isFuelRefillStarted = false;
+
+
+    void Awake()
+    {
+        maxFuel = 100;
+        currentFuel = maxFuel;
+        SetFuel(currentFuel);
+        shipControl = GetComponent<ShipControl>();
+        playerInput = GetComponent<PlayerInput>();
+        fuelConsumption = 1;
+        fuelRefill = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetFuel(currentFuel);
+        if (currentFuel <= 0) OutofGas();
+        if (currentFuel > 0 && playerInput.thrustInput) DrainFuel();
+        if (!playerInput.thrustInput) StopDrainingFuel();
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (!isFuelRefillStarted && collision.gameObject.tag == "FuelStation")
+        {
+            RefillFuel();
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "FuelStation")
+        {
+            StopRefillingFuel();
+        }
+    }
+
+    //this method sets the slider value to reflect the fuel amount
+    public void SetFuel(int currentFuel)
+    {
+        slider.value = currentFuel;
+    }
+
+    //this method drains fuel over time relative to a fuel consumption rate
+    public void DrainFuel()
+    {
+        if (!isFuelConsumptionStarted)
+        {
+            StartCoroutine(co_DrainFuel());
+            isFuelConsumptionStarted = true;
+        }
+    }
+
+    public void StopDrainingFuel()
+    {
+        StopCoroutine(co_DrainFuel());
+        isFuelConsumptionStarted = false;
+    }
+
+    //this coroutine drains fuel
+    public IEnumerator co_DrainFuel()
+    {
+        while (currentFuel > 0 && playerInput.thrustInput)
+        {
+            currentFuel -= fuelConsumption;
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+    void OutofGas()
+    {
+        shipControl.animator.SetBool("isThrusting", false);
+        FindObjectOfType<AudioManager>().Stop("EngineSound");
+    }
+
+    //this function starts the RefillFuel coroutine
+    public void RefillFuel()
+    {
+        if (!isFuelRefillStarted)
+        {
+            isFuelRefillStarted = true;
+            StartCoroutine(co_RefillFuel());
+        }
+    }
+
+    //this function stops the refuelling coroutine and toggles the isRefillStarted bool to true
+    public void StopRefillingFuel()
+    {
+        StopCoroutine(co_RefillFuel());
+        isFuelRefillStarted = false;
+    }
+
+    //this coroutine refills fuel
+    public IEnumerator co_RefillFuel()
+    {
+        while (currentFuel < 100 && !playerInput.thrustInput)
+        {
+            isFuelRefillStarted = true;
+            currentFuel += fuelRefill;
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Ship/ShipControl.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//these fields create the components in parenthesis if they are not found
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(PlayerInput))]
+public class ShipControl : MonoBehaviour
+{
+    //<PUBLIC VARIABLES>
+    //isControlling is used to check if the player can control the ship
+    public bool isControlling;
+    //thrust refers to the engine power and is the force applied when thrusting the engine
+    public float thrust;
+    //torque refers to the amount of angular momentum added to the ship when rotating clockwise and counterclockwise
+    public float torque;
+    //cargo is on the cargo which you drop to make deliveries
+    private Rigidbody2D cargo;
+    //animator is used to animate the ships thrust
+    public Animator animator;
+    //cargoDropLocation points to the location from where the cargo gets instantiated and dropped
+    public Transform cargoDropLocation;
+    //setDeliveryTarget is a reference to the setDeliveryTarget script which randomly chooses a residence as the destination for the delivery
+    public SetDeliveryTarget setDeliveryTarget;
+    //setNewTarget is used to check if the player can pick up more cargo
+    public bool setNewTarget;
+    //altitude text field is used to display current altitude on the UI altimeter
+    public Text altitude;
+    //this references which special is currently active
+    public int currentSpecial;
+    //this refers to the speed at which you can crash before exploding
+    public float crashTolerance = 4;
+
+    //<PRIVATE VARIABLES>
+    //rocketBlast area effector creates the force that blasts debris simulating engine blast
+    private AreaEffector2D rocketBlast;
+    //ship rigidbody is used to apply physics to the ship
+    private Rigidbody2D ship;
+    //shipTransform is used to determine the altitude of the ship
+    private Transform shipTransform;
+    //hasCargo gets set within the 'LandedScript' when you pick up or drop off a delivery
+    private bool hasCargo;
+    //reference to the FuelSystem script
+    public FuelSystem fuelSystem;
+    //reference to the PlayerInput script
+    private PlayerInput playerInput;
+    //explosionprefabs are particle systems
+    [SerializeField] private GameObject explosionprefab;
+    [SerializeField] private GameObject explosionprefab2;
+    private enum EngineStatus
+    {
+        on, off
+    }
+    private EngineStatus engineStatus;
+
+
+    public Rigidbody2D Cargo { get => cargo; private set => cargo = value; }
+
+    private void Awake()
+    {
+        fuelSystem = GetComponent<FuelSystem>();
+        playerInput = GetComponent<PlayerInput>();
+        ship = GetComponent<Rigidbody2D>();
+        shipTransform = GetComponent<Transform>();
+        cargoDropLocation = GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+        rocketBlast = GetComponentInChildren<AreaEffector2D>();
+        isControlling = true;
+        hasCargo = false;
+        setNewTarget = true;
+        currentSpecial = 1;
+    }
+
+    void Update()
+    {
+        altitude.text = Mathf.Round(shipTransform.position.y) + "m";
+        if (!isControlling) return;
+        if(playerInput.thrustInput) EngineOn();
+        if (playerInput.useSpecial) UseSpecial();
+        if (!playerInput.thrustInput) EngineOff();
+        if (hasCargo && playerInput.cargoDrop) DropCargo();
+    }
+
+    void FixedUpdate()
+    {
+        if (!isControlling) return;
+        if (playerInput.torqueInputR) TorqueR();
+        if (playerInput.torqueInputL) TorqueL();
+        if (fuelSystem.currentFuel > 0 && playerInput.thrustInput) Thrust();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bounce") Bounce();
+        else if (collision.relativeVelocity.magnitude >= crashTolerance) Crash();
+        else if (collision.gameObject.tag == "Restaurant" && setNewTarget) LoadPackage();
+    }
+
+    void EngineOn()
+    {
+        if (engineStatus == EngineStatus.on) return;
+        animator.SetBool("isThrusting", true);
+        rocketBlast.enabled = true;
+        FindObjectOfType<AudioManager>().Play("EngineSound");
+        engineStatus = EngineStatus.on;
+    }
+
+    void Thrust()
+    {
+        ship.AddForce(transform.up * thrust);
+    }
+
+    void TorqueL()
+    {
+        ship.AddTorque(torque * Mathf.Deg2Rad, ForceMode2D.Impulse);
+    }
+
+    void TorqueR()
+    {
+        ship.AddTorque(-torque * Mathf.Deg2Rad, ForceMode2D.Impulse);
+    }
+
+    void EngineOff()
+    {
+        if (engineStatus == EngineStatus.off) return;
+        animator.SetBool("isThrusting", false);
+        FindObjectOfType<AudioManager>().Stop("EngineSound");
+        rocketBlast.enabled = false;
+        engineStatus = EngineStatus.off;
+    }
+
+    void DropCargo()
+    {
+        Instantiate(Cargo, cargoDropLocation.position, cargoDropLocation.rotation);
+        hasCargo = false;
+        setNewTarget = true;
+    }
+
+    public void UseSpecial()
+    {
+    //    Debug.Log("special");
+    }
+
+    void Crash()
+    {
+        isControlling = false;
+        animator.SetBool("isThrusting", false);
+        FindObjectOfType<AudioManager>().Stop("EngineSound");
+        rocketBlast.enabled = false;
+        ship.angularDrag = 0.3f;
+        Destroy(gameObject);
+        FindObjectOfType<AudioManager>().Play("Explosion");
+        Instantiate(explosionprefab, gameObject.transform.position, Quaternion.identity);
+        Instantiate(explosionprefab2, gameObject.transform.position, Quaternion.identity);
+    }
+
+    void Bounce()
+    {
+        Debug.Log("Bounce Bby");
+    }
+
+    void LoadPackage()
+    {
+        hasCargo = true;
+        setDeliveryTarget.SetTarget();
+        setNewTarget = false;
+    }
+}
